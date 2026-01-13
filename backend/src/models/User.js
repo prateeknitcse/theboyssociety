1aeee8e12ef6bb0be64734495f599e4e5019e04b
prateeksingh <prateeksingh010101@gmail.com>
2026-01-13 23:57:01 +0530

    changed logic

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,20 +1,28 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
     role: {
       type: String,
-      enum: ["member", "admin"],
+      enum: ["admin", "member"],
       default: "member",
     },
-
-    dateOfBirth: { type: Date },
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.matchPassword = function (entered) {
+  return bcrypt.compare(entered, this.password);
+};
 
 export default mongoose.model("User", userSchema);
